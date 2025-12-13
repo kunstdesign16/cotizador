d8154de41f480eb519646f89e7e7f4d26d5d07b4
Mayela Manzano <kunstdesign@MacBook-Air-de-Mayela.local>
2025-12-13 14:32:08 -0600

    Fix: Force dynamic for ALL API routes

--- src/app/api/backup/import/route.ts
+++ src/app/api/backup/import/route.ts
@@ -2,6 +2,8 @@ import { prisma } from '@/lib/prisma'
 import { NextResponse } from 'next/server'
 import { revalidatePath } from 'next/cache'
 
+export const dynamic = 'force-dynamic'
+
 export async function POST(req: Request) {
     try {
         const formData = await req.formData()
